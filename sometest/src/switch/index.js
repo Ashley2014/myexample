b779f3d6a20553265d1f2181a0ef50ade58ae22b
xiaoming <1746534101@qq.com>
2018-01-04 16:38:09 +0800

    1

---
+++ sometest/src/switch/index.js
@@ -0,0 +1,16 @@
+let id=1
+
+switch (id){
+  case 1:
+    console.log(id)
+  case 2:
+    console.log(id)
+  case 3:
+    console.log(id)
+  case 1:
+  case 2:
+  case 3:
+    console.log('finally',id)
+    break;
+}
+
