5bc2fc5ee5755f64e410c68e87c05d2e708e03b4
Ashley2014 <1746534101@qq.com>
2018-04-09 14:01:29 +0800

    test

--- sometest/src/sku/sku2.js
+++ sometest/src/sku/sku2.js
@@ -1,245 +1,57 @@
 console.log('sku')
 
 let samples=[
-  {name:'布料',item:[{name:'棉'},{name:'涤纶'}],},
-  {name:'尺码',item:[{name:'m'},{name:'l'},{name:'s'}],},
-  {name:'颜色',item:[{name:'白色'},{name:'黑色'}],},
+  ['red','blue'],
+  ['m','l'],
+  ['silk','leather'],
 ]
 
-/*
-expect
- */
-let expectResult=[
-  {
-    name:'棉',
-    item:[
-      {
-        name:'m',item:[
-          {name:'白色',item:[]},
-          {name:'黑色',item:[]},
-        ]
-      },
-      {
-        name:'l',item:[
-          {name:'白色',item:[]},
-          {name:'黑色',item:[]},
-        ]
-      },
-      {
-        name:'s',item:[
-          {name:'白色',item:[]},
-          {name:'黑色',item:[]},
-        ]
-      },
-    ]
-  },
-  {
-    name:'涤纶',
-    item:[
-      {
-        name:'m',item:[
-          {name:'白色',item:[]},
-          {name:'黑色',item:[]},
-        ]
-      },
-      {
-        name:'l',item:[
-          {name:'白色',item:[]},
-          {name:'黑色',item:[]},
-        ]
-      },
-      {
-        name:'s',item:[
-          {name:'白色',item:[]},
-          {name:'黑色',item:[]},
-        ]
-      },
-    ]
-  },
-]
-
-let expectResult2=[
-  {
-    name:['棉','m','白色',],
-    store:12,
-    cover:'sample',
-  },
-  {
-    name:['棉','m','黑色',]
-  },
-  {
-    name:['棉','l','白色',]
-  },
-  {
-    name:['棉','l','黑色',]
-  },
-  {
-    name:['棉','s','白色',]
-  },
-  {
-    name:['棉','s','黑色',]
-  },
-  {
-    name:['涤纶','m','白色',]
-  },
-  {
-    name:['涤纶','m','黑色',]
-  },
-  {
-    name:['涤纶','l','白色',]
-  },
-  {
-    name:['涤纶','l','黑色',]
-  },
-  {
-    name:['涤纶','s','白色',]
-  },
-  {
-    name:['涤纶','s','黑色',]
-  },
+let samples2=[
+  [{name:'red'},{name:'blue'}],
+  [{name:'m'},{name:'l'}],
+  [{name:'silk'},{name:'leather'}],
 ]
 
 
-
-function makeSkutmp(arr) {
-  let out=[]
-  let cache=arr.map(n=>false)
-  arr.forEach((spec,i)=>{
-    putInResult(spec,out,i)
-  })
-  return out
-  function putInResult(spec,result,specIndex) {
-    // console.log('spec',spec)
-    // console.log('result',result)
-
-    // //0
-    // spec.item.forEach(n=>{
-    //   result.push(Object.assign({},n,{item:[]}))
-    // })
-    //
-    // //1
-    // spec.item.forEach(n=>{
-    //   result.item.forEach((n2)=>{
-    //     n2.push(Object.assign({},n,{item:[]}))
-    //   })
-    // })
-    //
-
-    // console.log(specIndex)
-
-    if(result.length<spec.item.length){
-      spec.item.forEach(n=>{
-        result.push(Object.assign({},n,{item:[]}))
-      })
-    }else{
-      result.forEach((n)=>{
-        putInResult(spec,n.item,specIndex)
-      })
-    }
-
-
-
-
-    spec.item.forEach(n=>{
-      result.push(Object.assign({},n,{item:[]}))
-    })
-
-
-  }
-
-  function putInResult2(spec,result) {
-    spec.item.forEach(n=>{
-      result.push(Object.assign({},n,{item:[]}))
-    })
-  }
-}
+let exceptResalut=[
+  ['red','m','silk'],
+  ['red','m','leather'],
+  ['red','l','silk'],
+  ['red','l','leather'],
+  ['blue','m','silk'],
+  ['blue','m','leather'],
+  ['blue','l','silk'],
+  ['blue','l','leather'],
+]
+console.log(makeSku(samples))
+console.log(makeSku(samples2))
 
 
 function makeSku(arr) {
-  let out=[]
-  arr.forEach((spec,i)=>{
-    putIn(spec,out,i)
-  })
-  return out
-  function putIn(spec,result) {
-    if(!result.length){
-      spec.item.forEach(n=>{
-        result.push(Object.assign({},n,{item:[]}))
-        // result.push({
-        //   ...n,
-        //   item:[],
-        // });
-        // result.push({
-        //   ...JSON.parse(JSON.stringify(n)),
-        //   item:[],
-        // });
-      })
-    }else{
-      result.forEach((n)=>{
-        putIn(spec,n.item)
-      })
-    }
-  }
+  let list = [];
+  recursionSub(list, arr.length, arr, 0, -1);
+  return list
 }
 
-
-
-function makeSku2(arr) {
-  let out=[]
-  arr.forEach((spec,i)=>{
-    putIn(spec,out)
-  })
-  return out
-  function putIn(spec,result) {
-    result
-    if(!result.length){
-      spec.item.forEach(n=>{
-        result.push(Object.assign({},n,{item:[]}))
-      })
-    }else{
-      result.forEach((n)=>{
-        putIn(spec,n.item)
-      })
-    }
+function recursionSub(list, count, array, ind, start, indexs) {
+  start++;
+  if (start > count - 1) {
+    return;
   }
-}
-
-
-
-
-
-let r1sku=makeSku(samples)
-console.log('1',r1sku)
-let f1sku=flattenSku(r1sku)
-console.log(2,f1sku)
-
-
-function flattenSku(arr) {
-  let out=[]
-  arr.forEach(n=>{
-    takeOut(n.item,out,[n])
-  })
-  return out
-  function takeOut(arr,result,names=[]) {
-    // console.log(arr)
-    arr.forEach(n=>{
-      if(n.item.length){
-        takeOut(n.item,result,names.concat(n))
-      }else{
-        result.push(Object.assign({},n,{
-          names:names.concat(n),
-        }))
+  if (start === 0) {
+    indexs = new Array(array.length);
+  }
+  for (indexs[start] = 0; indexs[start] < array[start].length; indexs[start]++) {
+    recursionSub(list, count, array, 0, start, indexs);
+    if (start === count - 1) {
+      let temp = new Array(count);
+      for (let i = count - 1; i >= 0; i--) {
+        temp[start - i] = array[start - i][indexs[start - i]];
       }
-    })
+      list.push(temp);
+    }
   }
 }
 
 
-describe("data string compare method", function() {
 
-
-  // it("'test", function() {
-
-    // expect(makeSku(samples)).toEqual(expectResult);
-  // });
-});
